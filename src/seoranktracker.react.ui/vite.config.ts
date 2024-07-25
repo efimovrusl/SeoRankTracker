0db7d834d4cdcdd05957dbeeb6c44bf2427972f9
yefimov.ruslan <efimovrusl@gmail.com>
2024-07-25 04:48:10 +0100

    Initialised and structured base projects

---
+++ src/seoranktracker.react.ui/vite.config.ts
@@ -0,0 +1,63 @@
+import {fileURLToPath, URL} from 'node:url';
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+import process from 'process';
+import child_process from 'child_process';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const baseFolder =
+    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+        ? `${process.env.APPDATA}/ASP.NET/https`
+        : `${process.env.HOME}/.aspnet/https`;
+
+const certificateName = "SeoRankTracker.React.UI";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+// Ensure the certificate files exist
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (0 !== child_process.spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], {stdio: 'inherit'}).status) {
+        throw new Error("Could not create certificate.");
+    }
+}
+
+// Use VITE_API_URL from the .env file
+const target = process.env.VITE_API_URL || 'https://localhost:7206';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            "/api": {
+                target: `https://localhost:${process.env.VITE_API_PORT}`,
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, "/api"),
+            },
+        },
+        port: 5173,
+        https: {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+        }
+    }
+});
